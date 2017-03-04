93573044dc52050975d6213bc006b24d3bddb11a
Kyle Machulis <kyle@nonpolynomial.com>
2017-03-03 19:08:03 -0800

    Remove caption and static directory config

--- conf.py
+++ conf.py
@@ -104,7 +104,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
@@ -163,11 +163,11 @@ latex_elements = {
 #      'Miscellaneous'),
 # ]
 
-# import sphinx_rtd_theme
+import sphinx_rtd_theme
 
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # At the bottom of conf.py
 def setup(app):
